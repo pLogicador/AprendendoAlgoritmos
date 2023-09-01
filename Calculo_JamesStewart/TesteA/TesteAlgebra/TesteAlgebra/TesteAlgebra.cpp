86027a5b0ce475f72cbf1f5faf4b9e88dfeec3c9
Pedro Miranda <pedroemanuelsm0@outlook.com>
2023-09-01 10:55:14 -0300

    Enviando material de revisão sobre algebra

---
+++ Calculo_JamesStewart/TesteA/TesteAlgebra/TesteAlgebra/TesteAlgebra.cpp
@@ -0,0 +1,176 @@
+﻿/*
+
+Revisão de álgebra:
+
+POTENCIAÇÃO
+1. Avalie cada expressão sem usar uma calculadora
+   a) (-3)^4              b)-3^4              c) 3^-4
+   d) 5^23/5^21           e)(2/3)^-2          f) 16^-3/4
+
+       Resolução passo a passo da letra a:
+
+       DICA: (base)^expoente
+       - Se você tem uma potência de base negativa e um expoente par(positivo) o resultado sempre será positivo.
+         Em outro caso, se o expoente for ímpar e a base negativa o resultado também será negativo.
+
+       Ra)
+         1. (-3)^4  =  (-3) * (-3) * (-3) * (-3)
+
+         2.             -   *  -   *  -   *  -   = +(Resultado Positivo)
+
+         3.             3   *  3   *  3   *  3   = 81
+         R. 81;
+
+            
+
+      Resolução passo a passo da letra b:
+      Rb)
+        1. -3^4  =  -( 3 * 3  3 * 3 ) = -81
+
+        R. -81;
+
+      Resolução passo a passo da letra c:
+
+      DICA: Para uma potência com um expoente negativo
+      Fórmula: [ a^-n  =  1/a^n ]
+    
+      Rc)
+        1. 3^-4   =  1/3^4
+
+        2. 1/3^4  =  1/81         (pois 3^4 = 81)
+
+        R. 1/81
+
+
+      Resolução passo a passo da letra d:
+
+      DICA: Para Divisão de potências de mesma base
+      Fórmula: [ a^n/a^m  =  a^n-m ]
+
+      Rd)
+        1. 5^23/5^21 = 5^2
+
+        2. 5^2 = 25
+
+        R. 25;
+
+
+       Resolução passo a passo da letra e:
+
+       DICA1: Inverte a base e troca o sinal do expoente
+       fórumla: [ (a/b)^-n  =  (b/a)^n ]
+
+       DICA2: A potência do cociente é o cociente das potências
+       fórmula: [ (a/b)^n  =  a^n/b^n ]
+
+       Re)
+         1. (2/3)^-2  =  (3/2)^2
+
+         2. (3/2)^2   =  3^2/2^2
+
+         3. 3^2/2^2   =  9/4        (pois 3^2 = 9 e 2^2 = 4)
+
+         R. 9/4;
+
+
+       Resolução passo a passo da letra f:
+
+       DICA1: fatoração
+           16 | 2
+           8  | 2
+           4  | 2
+           2  | 2
+           1    = 2^4
+
+       DICA2: Potência de Potência
+       fórmula: [  (a^n)^m  =  a^n*m  ]
+
+       Rf)
+         1. 16^-3/4     =   (2^4)^-3/4   (pois 16 = 2^4)
+
+         2. 2^4*(-3/4)  =   2^-3         (pois simplificou 4 por 4)
+
+         3. 2^-3        =   1/2^3
+
+         4. 1/2^3       =   1/8          (pois 2^3 = 8)
+
+         R. 1/8
+         
+         
+         
+ 2. Simplifique cada expressão. Escreva sua resposta sem expoentes negativos. 
+ a) √200 - √32 
+ b) (3a^3 * b^3) * (4ab^2)^2
+ c) ((3x^3/2 * y^3)/x^2 * y^-1/2)^-2
+         
+ 
+        Resolução passo a passo da letra a:
+
+        DICA1: Fatoração
+        200 | 2
+        100 | 2
+        50  | 2
+        25  | 5
+        5   | 5
+        1      = 2^2 * 2 * 5^2 (pois é uma raíz quadrada)
+
+        32 | 2
+        16 | 2
+        8  | 2
+        4  | 2
+        2  | 2
+        1     =  2^2 * 2^2 * 2 
+
+        DICA2: O que está elevado ao quadrado dentro da raíz quadrada deve sair da raíz (apenas se houver
+        produtos dentro da própria raíz)
+
+        Ra)
+          1. √200 - √32  =  √2^2*2*5^2  -  √2^2*2^2*2
+
+          2. √2^2*2*5^2  -  √2^2*2^2*2  =  2*5√2  -  2*2√2
+
+          3. 2*5√2  -  2*2√2  =  10√2  - 4√2
+
+          4. 10√2  - 4√2  = 6√2   (pois 10x - 4x = 6x)
+
+
+        Resolução passo a passo da letra b:
+
+        DICA1: A potência do produto é o produto das potências mas a potência da soma não é soma das potências
+        fórmula: [ (a*b)^n  =  a^n * b^n  ]
+
+        DICA2: Multiplicação de potências com mesma base(repete a base e soma os expoentes)
+
+        Rb)
+          1. (3a^3 * b^3) * (4ab^2)^2               =  3a^3 * b^3  *  4^2 * a^2 * (b^2)^2
+
+          2. 3a^3 * b^3  *  4^2 * a^2 * (b^2)^2     =  3 * a^5 * b^3   *   16 * b^4     (pois a^3 * a^2 = a^3+2; 4^4 = 16 ; (b^2)^2 = b^2*2)
+
+          3. 3 * a^5 * b^3   *   16 * b^4           =  48a^5 * b^7   (pois 3*16 = 48; b^3 * b^4 = b^3+4)
+
+          R. 48a^5 * b^7  
+ 
+ 
+
+        Resolução passo a passo da letra c:
+
+        DICA: A potência do produto é o produto das potências
+        fórmula: [ (a*b)^n = a^n * b^n ]
+
+        Rc)
+          1. ((3x^3/2 * y^3)/x^2 * y^-1/2)^-2       =  ( 3 * x^3/2 - 2  * y^3 - (-1/2) )^-2
+          2. ( 3 * x^3/2 - 2  * y^3 - (-1/2) )^-2   =  ( 3x^-1/2 * y^3 + 1/2 )^-2          (pois no x o exp 3/2 - 2 = 2*(-2) = -4 + 3 = -1)
+          3. (3x^-1/2 * y^3 + 1/2)^2                =  ( 3x^-1/2 * y^7/2 )^-2              (pois no y o exp 3 + 1/2 = 2*3=6+1 = 7)
+          4. ( 3x^-1/2 * y^7/2 )^-2                 =  3^-2 * (x^-1/2)^-2 * (y^7/2 )^-2  
+          5. 3^-2 * (x^-1/2)^-2 * (y^7/2 )^-2       =  3^-2 * x * y^-7                     (pois no x o exp -1/2 * -2 = 1; pois no y o exp 7/2 * -2 = -7  )
+          6. 3^-2 * x * y^-7                        =  1/3^2 * x * 1/3^7                   (pois invertemos a base)
+          7. 1/3^2 * x * 1/3^7                      =  x/9y^7                              (pois 1 * x * 1 = x)
+ */
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Teste de álgebra! \n";
+}
